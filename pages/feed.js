11c0dc6aa15401c335ace5a0622c484e688ef3a9
karan_c <karanchotaliya99@gmail.com>
2021-12-17 23:08:38 +0530

    project created

---
+++ pages/feed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Feed(props){
+	const [tweetList, setTweetList] = useState([])
+	const [tweetLoading, setTweetLoading] = useState(false)
+	
+	return(
+		<div className='feed-container'>
+
+		</div>
+	)
+}
