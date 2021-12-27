84a88bdbc55b783d6578b553f5566ac61d9c1d44
karan_c <karanchotaliya99@gmail.com>
2021-12-27 23:34:51 +0530

    feed page created

---
+++ components/Tweet.js
@@ -0,0 +1,33 @@
+import moment from 'moment'
+import React, { useState, useEffect } from 'react'
+
+
+export default function Tweet({ tweetData }){
+    return (
+        <div className='tweet-block'>
+            <div className='d-flex align-items-end'>
+                <div className='user-full-name'>
+                    {`${tweetData.user.first_name} ${tweetData.user.last_name}`}
+                </div>
+                <div className='username'>
+                    {`@${tweetData.user.user_name}`}
+                </div>
+                <div className='date'>
+                    {moment(tweetData.timestamp).fromNow()}
+                </div>
+            </div>
+            <div className='tweet-text'>
+                {tweetData.content}
+            </div>
+            <div>
+                {tweetData.retweet_obj && <Tweet tweetData={tweetData.retweet_obj}/>}
+            </div>
+            <div className='d-flex mt-2'>
+                <div className='like-count'>
+                    {tweetData.likes} Likes
+                </div>
+                <div className='rt-count'></div>
+            </div>
+        </div>
+    )
+}
--- pages/feed.js
+++ pages/feed.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Tweet from '../components/Tweet';
 
 export default function Feed(props){
 	const [tweetList, setTweetList] = useState([])
 	const [tweetLoading, setTweetLoading] = useState(false)
 	
+	useEffect(() => {
+		fetchTweets()
+	}, [])
+
+	const fetchTweets = async () => {
+		setTweetLoading(true)
+		let api = 'http://127.0.0.1:8000/api/tweet/'
+		try {
+			let data = await axios.get(api)
+			if (data.status === 200) {
+				setTweetList(data.data)
+			}
+			setTweetLoading(false)
+		}
+		catch(err) {
+			alert("Something went wrong!")
+			setTweetLoading(false)
+			console.log(err)
+		}
+	}
+
 	return(
 		<div className='feed-container'>
-
+			<div className='container'>
+				{tweetList.map((item, idx) => <Tweet tweetData={item} key={"tweet_" + idx}/>)}
+			</div>
 		</div>
 	)
 }
