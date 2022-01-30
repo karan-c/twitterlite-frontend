8381ca0cb6c1f12d87c5e9affed9d4e99faa317a
karan_c <karanchotaliya99@gmail.com>
2022-01-30 15:09:12 +0530

    build error resolved

--- components/StaticBlock.js
+++ components/StaticBlock.js
@@ -3,7 +3,7 @@ export default function StaticBlock(props) {
         <div className='static-block sticky-div'>
             <div className='text-1'>
                 Built by &nbsp;&nbsp;
-                <a href='https://github.com/karan-c' target="_blank">
+                <a href='https://github.com/karan-c' target="_blank" rel="noreferrer">
                     <i className='fa-brands fa-github'></i>&nbsp;
                     <u>karan-c</u>
                 </a>
@@ -11,12 +11,12 @@ export default function StaticBlock(props) {
             <div className='text-2'>
                 <div>Checkout the source code here</div>
                 <div className='d-flex mt-1' style={{gap: "10px"}}>
-                    <a href="https://github.com/karan-c/TwitterLite" target="_blank">
+                    <a href="https://github.com/karan-c/TwitterLite" target="_blank" rel="noreferrer">
                         <div className='tag'>
                             <i className='fa-solid fa-code'></i> Back-end
                         </div>
                     </a>
-                    <a href="https://github.com/karan-c/twitterlite-frontend" target="_blank">
+                    <a href="https://github.com/karan-c/twitterlite-frontend" target="_blank" rel="noreferrer">
                         <div className='tag'>
                             <i className='fa-solid fa-code'></i> Front-end
                         </div>
